b82ded759140c79e9be9a172bdf8babdbe7ca651
ankiiitraj <ankitatiiitr@gmail.com>
2020-08-13 17:46:12 +0530

    Updates

---
+++ precache-manifest.4d4e0cdb5b390c5fa9fa5da825edcfff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5cd15d0a480e57c88eb804e5d8f1d6",
+    "url": "/sorting-wait/index.html"
+  },
+  {
+    "revision": "5f313c8f88fd60c6fae8",
+    "url": "/sorting-wait/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6c94cef12a5d9a9d62ab",
+    "url": "/sorting-wait/static/js/2.2f4cd49a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sorting-wait/static/js/2.2f4cd49a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f313c8f88fd60c6fae8",
+    "url": "/sorting-wait/static/js/main.8473b3c2.chunk.js"
+  },
+  {
+    "revision": "0c509699c96c66c9f1e3",
+    "url": "/sorting-wait/static/js/runtime-main.e079d158.js"
+  }
+]);
