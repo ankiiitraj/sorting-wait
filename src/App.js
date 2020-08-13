ade6b264918de90ccabb449698ddc036dd94f21d
ankiiitraj <ankitatiiitr@gmail.com>
2020-08-13 17:55:24 +0530

    sorting visuallizer initialized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Visualizer from './components/visualizer'
+
+function App() {
+  return (
+    <div className="App">
+      <Visualizer />
+    </div>
+  );
+}
+
+export default App;
